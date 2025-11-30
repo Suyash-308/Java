39902ee80415de729a4e36e8aad4a780ab80dc48
Suyashnichit308 <nichitsuyash@gmail.com>
2025-11-30 22:15:46 +0530

    added basic  examples

---
+++ src/Demo_1.java
@@ -0,0 +1,13 @@
+public class Demo_1 {
+    public static void main(String[] args) {
+        System.out.println(10+10);
+        System.out.println(10+'A');
+        System.out.println('A'+'A');
+        System.out.println(10+"A");
+        System.out.println("A"+"A");
+        System.out.println("A"+'A');
+        System.out.println("A"+10);
+        System.out.println(10+"A"+10);
+        System.out.println('A'+"A");
+    }
+}
