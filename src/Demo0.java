c934d0025e98d29b892acfa38726e34912ef8098
Suyashnichit308 <nichitsuyash@gmail.com>
2025-12-23 10:08:23 +0530

    practice

---
+++ src/Demo0.java
@@ -0,0 +1,14 @@
+public class Demo0 {
+    public static void main(String[] args) {
+        int num=1334562;
+        int count=0;
+
+            while (num!=0){
+                num=num/10;
+                count++;
+
+        }
+            System.out.println(count);
+
+    }
+}
