edd78a9e4717e300cb4bc065b23b6a34d561c631
Suyashnichit308 <nichitsuyash@gmail.com>
2026-01-17 10:58:19 +0530

    practice

---
+++ src/DemoMethod.java
@@ -0,0 +1,13 @@
+public class DemoMethod {
+    public static void main(String[] args) {
+
+        A a=new A();
+        a.m1();
+        }
+    }
+    class  A{
+
+    public void m1(){
+        System.out.println("m1 methos");
+    }
+    }
