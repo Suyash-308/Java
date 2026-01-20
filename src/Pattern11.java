01440b958bae6dfb0db2e3adaa3fe3c72d0d0d45
Suyashnichit308 <nichitsuyash@gmail.com>
2026-01-20 17:18:03 +0530

    Advance pattern

---
+++ src/Pattern11.java
@@ -0,0 +1,16 @@
+public class Pattern11 {
+    static void main() {
+        int n=5;
+        for (int i = 1; i <=n ; i++) {
+            int space=n-i;
+            for (int j = 1; j <=space ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+    }
+}
