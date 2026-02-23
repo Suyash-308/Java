104504ee61a5de1e6dfbdb0a50043d168df6b185
Suyashnichit308 <nichitsuyash@gmail.com>
2026-02-23 23:10:37 +0530

    Pattern

---
+++ src/Pattern/Pattern6.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class Pattern6 {
+    static void main() {
+        int n=5;
+        for (int i =1; i <=n ; i++) {
+            for (int j =1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=5 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
