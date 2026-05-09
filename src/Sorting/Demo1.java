ac595e6fb50044f4b28fe26dd24d002c08f75865
Suyashnichit308 <nichitsuyash@gmail.com>
2026-05-09 10:26:42 +0530

    Bubble Sort

--- src/Sorting/Demo1.java
+++ src/Sorting/Demo1.java
@@ -1,12 +1,12 @@
 package Sorting;
 //Selection Sort
-public class Demo1 {
+public class  Demo1 {
     static void printArray(int[] arr){
         for (int i = 0; i < arr.length ; i++) {
             System.out.println(arr[i]);
         }
     }
-    static void main() {
+   public static void main(String[] args) {
         int[] arr={7,8,3,2,1};
 
         for (int i = 0; i <arr.length-1 ; i++) {
