223ac7d10ecb06be7dfb4d66306e203b256c2a1c
Suyashnichit308 <nichitsuyash@gmail.com>
2026-08-21 21:06:21 +0530

    practice

--- core-java/src/Demo_$1.java
+++ core-java/src/Demo_$1.java
@@ -1,6 +1,18 @@
+import practice.Student;
+
+import java.util.Arrays;
+
 public class Demo_$1 {
    public static void main(String[] args) {
-        String str =new String("Hii");
-       System.out.println(str);
-    }
+       String str = "Hii";
+       String str1 = "Hii";
+
+
+       System.out.println(str.hashCode());
+       System.out.println(str.concat(" Alice"));
+
+       System.out.println(str.hashCode() == str1.hashCode());
+       System.out.println(str.getClass());
+
+   }
 }
--- core-java/src/practice/Demo6.java
+++ core-java/src/practice/Demo6.java
@@ -11,22 +11,3 @@ public class Demo6 {
 
     }
 }
-class Student{
-    private int id;
-    private String name;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-   public int getId(){
-        return id;
-   }
-    public String getName(){
-        return name;
-    }
-}
